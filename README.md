9c233e21be258253326f37a6d32036d3e1674dac
moziada <60968402+moziada@users.noreply.github.com>
2023-03-12 18:52:30 +0200

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# AE_SRT2Text-Layer
-Adobe After Effects script that creates text layer sequence from SRT subtitle file
+# Introduction
+This code is made using [ExtendScript](https://extendscript.docsforadobe.dev/introduction/extendscript-overview.html) language with the help of [ChatGPT](https://openai.com/blog/chatgpt) to create text layers from existing ```.srt``` subtitle file in [Adobe After Effects](https://www.adobe.com/mena_en/products/aftereffects.html)
+
+# How It Works?
+The script creates a floating window with a "Select File" button that allows the user to browse for an ```.srt``` subtitle file. Once the user selects the file, the script reads the file and adds a text layer for each subtitle line to the active composition in After Effects.
+To use the script within After Effects place ```srt2textLayer.jsx``` script file into ```C:\Program Files\Adobe\Adobe After Effects 2020\Support Files\Scripts``` path and the script should be available in After Effects
+
+![demo](imgs/demo.gif)
+
+# Openai Chat
+Here is the chat and the process of making the script
+
+![chat](imgs/chat.openai.com.png)
---
+++ script/srt2textLayer.jsx
@@ -0,0 +1,74 @@
+﻿// Create a new window
+var myWindow = new Window("palette", "srt to text layers");
+
+// Create a label for the selected file
+var selectedFileLabel = myWindow.add("statictext", undefined, "Select .srt File");
+
+// Create a button that opens a file browser
+var selectFileButton = myWindow.add("button", undefined, "Browse");
+selectFileButton.onClick = function() {
+    var textYPos = app.project.activeItem.height - (app.project.activeItem.height * 0.25);
+    var textXPos = app.project.activeItem.width/2;
+    var srtFile = File.openDialog("Select .srt file", "*.srt");
+    if (srtFile != null) {
+        // Read the file line by line
+        srtFile.open("r");
+        srtFile.encoding = "UTF-8";
+        var startTime, endTime, text;
+        var state = "number";
+        while (!srtFile.eof) {
+            var line = srtFile.readln();
+            switch(state) {
+                case "number":
+                    // Skip the line containing the card number
+                    state = "timecodes";
+                    break;
+                case "timecodes":
+                    // This line contains the time codes
+                    var timeCodes = line.split(" --> ");
+                    startTime = timeToSeconds(timeCodes[0]);
+                    endTime = timeToSeconds(timeCodes[1]);
+                    state = "text";
+                    break;
+                case "text":
+                    // This line contains the text for the layer
+                    if (line === "") {
+                        // The line is empty, so we're done reading the text for this card
+                        // Add the text layer to the composition
+                        var textLayer = app.project.activeItem.layers.addText(text);
+                        textLayer.startTime = startTime;
+                        textLayer.outPoint = endTime;
+                        // Center the layer horizontally
+                        textLayer.justification = ParagraphJustification.CENTER_JUSTIFY;
+                        // Set the position of the layer
+                        textLayer.position.setValue([textXPos, textYPos]);
+                        //textLayer.sourcePointToComp([textXPos, textYPos]);
+                        
+                        state = "number";
+                        text = undefined; // Reset text variable
+                    } else {
+                        // Add the line to the text for this card
+                        if (text === undefined) {
+                            text = line;
+                        } else {
+                            text += "\r" + line;
+                        }
+                    }
+                    break;
+            }
+        }
+        srtFile.close();
+    }
+};
+
+// Show the window
+myWindow.show();
+
+// Function to convert a timecode string to seconds
+function timeToSeconds(timecode) {
+    var parts = timecode.split(":");
+    var hours = parseInt(parts[0]);
+    var minutes = parseInt(parts[1]);
+    var seconds = parseFloat(parts[2].replace(",", "."));
+    return (hours * 3600) + (minutes * 60) + seconds;
+}
